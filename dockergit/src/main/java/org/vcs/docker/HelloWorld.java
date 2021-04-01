c0096652550b512308325280e41734ab2621c2a4
Victor <victorcs333@gmail.com>
2021-04-01 18:45:47 -0500

    Commit fuentes

---
+++ dockergit/src/main/java/org/vcs/docker/HelloWorld.java
@@ -0,0 +1,12 @@
+package org.vcs.docker;
+
+public class HelloWorld {
+	
+	public static void main(String args[]) throws Exception{
+		for(int i = 0 ; i < 100; i++){
+			System.out.println("Hello World Ping " + i );
+			Thread.sleep(1000);
+		}
+	}
+
+}
